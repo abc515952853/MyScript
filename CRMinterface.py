828911b072c07d0cf08368d94b74478c35429851
abc515952853 <123456789@qq.com>
2018-12-07 11:20:22 +0800

    11

--- CRMinterface.py
+++ CRMinterface.py
@@ -2,7 +2,7 @@ import requests
 import  json 
 import uuid
 
-session = 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjBjNjM2MDJiNTMxOWU5Y2ZiMmY3Yzg2MGQyMzJiNjNiIiwidHlwIjoiSldUIn0.eyJuYmYiOjE1NDQwNjMxMTQsImV4cCI6MTU0NDE0OTUxNCwiaXNzIjoibnVsbCIsImF1ZCI6WyJudWxsL3Jlc291cmNlcyIsImNybSIsImRpbmd0YWxrIiwiaHIiXSwiY2xpZW50X2lkIjoiZGluZ3RhbGsiLCJzdWIiOiJjMWY2Nzc3MC04MTA0LTRhOGQtOTFmMy0zZTcwMjU0N2EzNjUiLCJhdXRoX3RpbWUiOjE1NDQwNjMxMTQsImlkcCI6ImxvY2FsIiwibmFtZSI6IuayiOaWjCIsImRpbmd0YWxrX3VzZXJpZCI6ImE4OGI5MjFhZjY2MDQzZDE4MGE5ZmVhMTRjMjE1YTg1Iiwic2NvcGUiOlsib3BlbmlkIiwicHJvZmlsZSIsImNybSIsImRpbmd0YWxrIiwiaHIiXSwiYW1yIjpbImRpbmd0YWxrX2NvZGUiXX0.Z2eE-vQ13KN5JTUD9E6yQ8pdMdS_HuSHH_xUAIU3TGH4N-jV9r7WJ1JgfwA87fwd2JzKZX9ZqR7PRx1CygvuTDmuxGLR04m1nb4u59Bx4w64kXqKeN5gkV5GmzP2tAslo3nnuHNtSo_zLqyjcyMRPaiVEEPN3XIw-Sqw5r44Ub3TeBz_glcgZ8QmoGETj1fsH5FgK0A5nQW-zkhVLHtjZPNipX0XVpjVM4kvxQ_Bfg_-wNYy8yIqrCJQLv_kk4zu0H6iA-zjs-vDEcJGen9QOkbRo1eTn3vs7T3JGoBKNs9UXb7a46ek6gKUubiBfelldSVbtXAvUhw9iUtdU7Erww'
+session = 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjBjNjM2MDJiNTMxOWU5Y2ZiMmY3Yzg2MGQyMzJiNjNiIiwidHlwIjoiSldUIn0.eyJuYmYiOjE1NDQxNDk0MjgsImV4cCI6MTU0NDIzNTgyOCwiaXNzIjoibnVsbCIsImF1ZCI6WyJudWxsL3Jlc291cmNlcyIsImNybSIsImRpbmd0YWxrIiwiaHIiXSwiY2xpZW50X2lkIjoiZGluZ3RhbGsiLCJzdWIiOiJiNzI2YTk1Mi04Mzc2LTRhN2ItODNlNC1kYWE1ZmFlNWE2YjkiLCJhdXRoX3RpbWUiOjE1NDQxNDk0MjgsImlkcCI6ImxvY2FsIiwibmFtZSI6IuW8oOi2hSIsImRpbmd0YWxrX3VzZXJpZCI6ImI0ZjcwYWEyYTcxNzQzNDI5NzMyZjAzZmU4MTQ2MDYyIiwic2NvcGUiOlsib3BlbmlkIiwicHJvZmlsZSIsImNybSIsImRpbmd0YWxrIiwiaHIiXSwiYW1yIjpbImRpbmd0YWxrX2NvZGUiXX0.ud3J28KxS7EnOmi_ufC-kJhES94_N5v1zFDbYoyDmZmEyK1vDbtk9BlBrty1q_89RzBNpPCTrgiBUvz_JS3bz5o2k-0lftXlzmHBJrsctLm6HV7jvB9D31jMtJZcNqJ0izo6jjavvmi-mt3QBfSoExwt5vacmjhV02cR6ofxaZcO_nDuwTnzmikKj6Ie-0p8yV7CSDZPLcVTCLcalNY8T5c8Zi2lO_NSmYA6fe465Ob1mx09Ppht-N0ehv7W32-OWxSPMkVQEozzEQNh-tAmi0BcgvOinx63z1fQbmvylDpsTYfBZbeeKJ8wVgjLXh3OyMakXMaVAPc8jCBcRjMPVw'
 requestid = str(uuid.uuid1())
 url = 'http://api.syzb.qianjifang.com.cn/'
 
@@ -15,20 +15,23 @@ url = 'http://api.syzb.qianjifang.com.cn/'
 # }
 # r = requests.post(url=apiurl, headers = headers,data = json.dumps(payload))
 # print(r.status_code)
-
-apiurl = url + 'api/Customer/create'
-headers = {'Content-Type': "application/json",'Authorization':session,"x-requestid":requestid}
-payload ={
-  "name": "杭州千之海科技有限公司",
-  "shortName": "千之海",
-  "street": "33",
-  "city": "浙江省",
-  "state": "杭州市",
-  "customerProspectId": 4,
-  "customerTypeId": 3
-}
-r = requests.post(url=apiurl, headers = headers,data = json.dumps(payload))
-print(r.status_code)
+i=2
+while i <= 50:
+  requestid = str(uuid.uuid1())
+  apiurl = url + 'api/Customer/create'
+  headers = {'Content-Type': "application/json",'Authorization':session,"x-requestid":requestid}
+  payload ={
+    "name": "阿超科技有限公司"+str(i),
+    "shortName": "阿超科技"+str(i),
+    "street": "33",
+    "city": "浙江省",
+    "state": "杭州市",
+    "customerProspectId": 4,
+    "customerTypeId": 3
+  }
+  r = requests.post(url=apiurl, headers = headers,data = json.dumps(payload))
+  print(r.status_code)
+  i=i+1
 
 
 # apiurl = url + 'api/Customer/AddSchedule'
